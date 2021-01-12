57cd7c042398a9de30e61458671cd1c0b9716e06
davi.goncalves <davigoncalves@movida.com.br>
2021-01-11 22:18:50 -0300

    P/L da ação

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 base_url = "https://statusinvest.com.br"
 all_stocks_path = "/acao/companiesnavigation?page=1&size=1000"
 
@@ -11,6 +12,6 @@ stock = requests.get(base_url+"/"+all_stocks.json()[0]['url'])
 
 soup = BeautifulSoup(stock.content, 'html.parser')
 
-data = soup.select(' div.top-info .info')
-print(data[0].contents[0])#<class 'bs4.element.NavigableString'>
-# print(data.contents[0])
+#Achando o P/L da ação
+PL = soup.find_all('div',attrs={'title': 'Dá uma ideia do quanto o mercado está disposto a pagar pelos lucros da empresa.'})[0].find('strong').string
+print(PL)
