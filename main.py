abc9bc7645066eed8b801a1201ed9e3a76837958
davi.goncalves <davigoncalves@movida.com.br>
2021-01-12 20:53:01 -0300

    Pegando o ROE da ação

--- main.py
+++ main.py
@@ -14,4 +14,9 @@ soup = BeautifulSoup(stock.content, 'html.parser')
 
 #Achando o P/L da ação
 PL = soup.find_all('div',attrs={'title': 'Dá uma ideia do quanto o mercado está disposto a pagar pelos lucros da empresa.'})[0].find('strong').string
+
+#Achando ROE da ação
+ROE = soup.find_all('div',attrs={'title': 'Mede a capacidade de agregar valor de uma empresa a partir de seus próprios recursos e do dinheiro de investidores.'})[0].find('strong').string
+
 print(PL)
+print(ROE)
